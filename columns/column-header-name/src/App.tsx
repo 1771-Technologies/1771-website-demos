7efab0e27a5560d26846f007c5f144b04495fd09
Lee <leebeydoun@1771technologies.com>
2025-05-06 06:30:37 +0100

    Alpha

---
+++ columns/column-header-name/src/App.tsx
@@ -0,0 +1,39 @@
+import {
+  LyteNyteGrid,
+  useLyteNytePro,
+  useClientDataSource,
+} from "@1771technologies/lytenyte-pro";
+import "@1771technologies/lytenyte-pro/grid.css";
+import { ColumnProReact } from "@1771technologies/lytenyte-pro/types";
+import { bankDataSmall } from "@1771technologies/sample-data/bank-data-smaller";
+import { useId } from "react";
+
+const columns: ColumnProReact[] = [
+  { headerName: "Years Alive", id: "age", type: "number" },
+  { headerName: "Employment", id: "job" },
+  { headerName: "Money In Bank", id: "balance", type: "number" },
+  { headerName: "Smartness Level", id: "education" },
+  { headerName: "Single Or Not?", id: "marital" },
+];
+
+export function App() {
+  const ds = useClientDataSource({ data: bankDataSmall });
+
+  const grid = useLyteNytePro({
+    gridId: useId(),
+    rowDataSource: ds,
+    columns,
+
+    columnBase: {
+      widthFlex: 1,
+    },
+  });
+
+  return (
+    <div style={{ height: 500, display: "flex", flexDirection: "column" }}>
+      <div style={{ flex: "1" }}>
+        <LyteNyteGrid grid={grid} />
+      </div>
+    </div>
+  );
+}
