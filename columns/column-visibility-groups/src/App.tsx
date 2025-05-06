34ae66724136c413ad8fa9f827a58bfb7bebffca
Lee <leebeydoun@1771technologies.com>
2025-05-06 07:15:00 +0100

    Alpha

---
+++ columns/column-visibility-groups/src/App.tsx
@@ -0,0 +1,61 @@
+import {
+  LyteNyteGrid,
+  useLyteNytePro,
+  useClientDataSource,
+} from "@1771technologies/lytenyte-pro";
+import "@1771technologies/lytenyte-pro/grid.css";
+import { ColumnProReact } from "@1771technologies/lytenyte-pro/types";
+import { bankDataSmall } from "@1771technologies/sample-data/bank-data-smaller";
+import { useId } from "react";
+
+const columns: ColumnProReact[] = [
+  { id: "age", type: "number", groupPath: ["One Group"] },
+  { id: "job", groupPath: ["One Group"] },
+  { id: "balance", type: "number", groupPath: ["One Group"] },
+  { id: "education", hide: true },
+  { id: "marital" },
+];
+
+export function App() {
+  const ds = useClientDataSource({ data: bankDataSmall });
+
+  const grid = useLyteNytePro({
+    gridId: useId(),
+    rowDataSource: ds,
+    columns,
+
+    columnBase: {
+      widthFlex: 1,
+    },
+  });
+
+  return (
+    <div style={{ height: 500, display: "flex", flexDirection: "column" }}>
+      <div>
+        <button
+          onClick={() =>
+            grid.api.columnUpdateMany({
+              education: { hide: false },
+              marital: { hide: false },
+            })
+          }
+        >
+          Show Education
+        </button>
+        <button
+          onClick={() =>
+            grid.api.columnUpdateMany(
+              Object.fromEntries(columns.map((c) => [c.id, { hide: true }]))
+            )
+          }
+        >
+          Hide All
+        </button>
+        <button onClick={() => grid.state.columns.set(columns)}>Reset</button>
+      </div>
+      <div style={{ flex: "1" }}>
+        <LyteNyteGrid grid={grid} />
+      </div>
+    </div>
+  );
+}
