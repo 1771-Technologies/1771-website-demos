3a80c3c0bbf9a03f6486dad9620a42cbea52a112
Lee <leebeydoun@1771technologies.com>
2025-05-07 23:05:23 +0100

    Alpha

--- row-data-source/client-row-data-source-dates/src/App.tsx
+++ row-data-source/client-row-data-source-dates/src/App.tsx
@@ -5,32 +5,33 @@ import {
 } from "@1771technologies/lytenyte-pro";
 import "@1771technologies/lytenyte-pro/grid.css";
 import { ColumnProReact } from "@1771technologies/lytenyte-pro/types";
-import { bankDataSmall } from "@1771technologies/sample-data/bank-data-smaller";
+import { companiesWithPricePerf } from "@1771technologies/sample-data/companies-with-price-performance";
 import { useId } from "react";
 
 const columns: ColumnProReact[] = [
-  { id: "age", type: "number" },
-  { id: "job" },
-  { id: "balance", type: "number" },
-  { id: "education" },
-  { id: "marital" },
-  { id: "default" },
-  { id: "housing" },
-  { id: "loan" },
-  { id: "contact" },
-  { id: "day", type: "number" },
-  { id: "month" },
-  { id: "duration" },
-  { id: "campaign" },
-  { id: "pdays" },
-  { id: "previous" },
-  { id: "poutcome" },
-  { id: "y" },
+  { id: "Company" },
+  { id: "Founded" },
+  { id: "Employee Cnt" },
+  { id: "Country" },
+  { id: "Price" },
+  {
+    id: "YearEnd",
+    headerName: "Year End",
+    sortable: true,
+    type: "date",
+    uiHints: { sortButton: true },
+  },
 ];
 
 export function App() {
   const ds = useClientDataSource({
-    data: bankDataSmall,
+    data: companiesWithPricePerf,
+
+    sortToDate: (c) => {
+      if (!c) return new Date();
+
+      return parseMonthDay(c as string);
+    },
   });
 
   const grid = useLyteNytePro({
@@ -46,4 +47,20 @@ export function App() {
       </div>
     </div>
   );
+}
+
+function parseMonthDay(
+  dateString: string,
+  year: number = new Date().getFullYear()
+): Date {
+  // Create a date from the string plus the current year
+  const date = new Date(`${dateString}, ${year}`);
+  console.log(date);
+
+  // Check if the date is valid
+  if (isNaN(date.getTime())) {
+    throw new Error("Invalid date string format");
+  }
+
+  return date;
 }
