0ae472f2d76aed8e18697a743373cfedf1dbd588
Lee <leebeydoun@1771technologies.com>
2025-05-08 07:38:55 +0100

    Alpha

--- row-data-source/server-row-data-source-row-grouping/src/App.tsx
+++ row-data-source/server-row-data-source-row-grouping/src/App.tsx
@@ -5,7 +5,7 @@ import {
 } from "@1771technologies/lytenyte-pro";
 import {
   AsyncDataResponseProReact,
-  DataFetcherParamsPropReact,
+  DataFetcherProReact,
 } from "@1771technologies/lytenyte-pro/types";
 import "@1771technologies/lytenyte-pro/grid.css";
 import { ColumnProReact } from "@1771technologies/lytenyte-pro/types";
@@ -15,9 +15,9 @@ import sql from "alasql";
 
 const columns: ColumnProReact[] = [
   { id: "age", type: "number" },
-  { id: "job" },
+  { id: "job", rowGroupable: true },
   { id: "balance", type: "number" },
-  { id: "education" },
+  { id: "education", rowGroupable: true },
   { id: "marital" },
   { id: "default" },
   { id: "housing" },
@@ -31,6 +31,7 @@ export function App() {
   const grid = useLyteNytePro({
     gridId: useId(),
     rowDataSource: ds,
+    rowGroupModel: ["job", "education"],
     columns,
   });
 
@@ -60,26 +61,76 @@ CREATE TABLE IF NOT EXISTS banks
 
 sql.tables.banks.data = bankDataSmall;
 
-const dataFetcher = async (p: DataFetcherParamsPropReact) => {
-  // An artificial pause to make it seem like a server request
+const dataFetcher: DataFetcherProReact = async (p) => {
   await new Promise<void>((res) => {
     setTimeout(() => res(), 500);
   });
 
   const blockSize = p.blockSize;
   const blocks: AsyncDataResponseProReact["blocks"] = [];
+  const state = p.api.getState();
+
+  const groupModel = state.rowGroupModel.peek();
+  const isGrouping = groupModel.length > 0;
 
   let rootCount: number | undefined = undefined;
-  rootCount = sql<{ cnt: number }[]>(`SELECT count(*) AS cnt FROM banks`)[0]
-    .cnt;
+  if (isGrouping) {
+    rootCount = sql<{ cnt: number }[]>(
+      `SELECT count(*) AS cnt FROM banks GROUP BY ${groupModel[0]}`
+    ).length;
+  } else {
+    rootCount = sql<{ cnt: number }[]>(`SELECT count(*) AS cnt FROM banks`)[0]
+      .cnt;
+  }
 
   for (let i = 0; i < p.requestBlocks.length; i++) {
     const b = p.requestBlocks[i];
 
-    const query = `SELECT * FROM banks LIMIT ${blockSize} OFFSET ${
+    const groupKey = groupModel[b.path.length];
+
+    // We have a row grouping so we need to group by item, offset and get count.
+    if (groupKey) {
+      const data = sql<{ childCnt: number; pathKey: string }[]>(
+        `SELECT *, ${groupKey} AS pathKey, count(*) AS childCnt FROM banks GROUP BY ${groupKey} LIMIT ${blockSize} OFFSET ${
+          blockSize * b.blockKey
+        }`
+      );
+      const cnt = sql<{ cnt: number }[]>(
+        `SELECT count(*) AS cnt FROM banks GROUP BY ${groupKey}`
+      ).length;
+
+      blocks.push({
+        blockKey: b.blockKey,
+        frame: {
+          childCounts: data.map((d) => d.childCnt),
+          data: data.map((c) => ({ ...c, pathKey: c.pathKey.toString() })),
+          ids: data.map(
+            (_, i) => `${i + b.blockKey * blockSize}__${b.path.join("/")}`
+          ),
+          kinds: data.map(() => 2),
+          pathKeys: data.map((c) => c.pathKey.toString()),
+        },
+        path: b.path,
+        size: cnt,
+      });
+      continue;
+    }
+
+    const whereParts = b.path
+      .map((c, i) => {
+        const column = p.api.columnById(groupModel[i])!;
+        return `${groupModel[i]} = ${
+          column.type === "number" ? Number.parseFloat(c) : `'${c}'`
+        }`;
+      })
+      .join(" AND ");
+
+    const whereClause = whereParts ? `WHERE ${whereParts}` : "";
+
+    const query = `SELECT * FROM banks ${whereClause} LIMIT ${blockSize} OFFSET ${
       b.blockKey * blockSize
     }`;
-    const cntQuery = `SELECT count(*) AS cnt FROM banks`;
+    const cntQuery = `SELECT count(*) AS cnt FROM banks ${whereClause}`;
 
     const data = sql(query) as unknown[];
     const count = sql<{ cnt: number }[]>(cntQuery)[0].cnt;
