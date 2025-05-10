473b5ce3c6e6d0980381f3866c8113249d456733
Lee <leebeydoun@1771technologies.com>
2025-05-10 13:08:15 +0100

    Alpha

---
+++ pivoting/row-grouping/src/App.tsx
@@ -0,0 +1,52 @@
+import {
+  LyteNyteGrid,
+  useLyteNytePro,
+  useClientDataSource,
+} from "@1771technologies/lytenyte-pro";
+import "@1771technologies/lytenyte-pro/grid.css";
+import { ColumnProReact } from "@1771technologies/lytenyte-pro/types";
+import { bankDataSmall } from "@1771technologies/sample-data/bank-data-smaller";
+import { useId } from "react";
+
+const columns: ColumnProReact[] = [
+  { id: "age", type: "number" },
+  { id: "job", rowGroupable: true },
+  { id: "balance", type: "number" },
+  { id: "education", rowGroupable: true },
+  { id: "marital" },
+  { id: "default" },
+  { id: "housing" },
+  { id: "loan" },
+  { id: "contact" },
+  { id: "day", type: "number" },
+  { id: "month" },
+  { id: "duration" },
+  { id: "campaign" },
+  { id: "pdays" },
+  { id: "previous" },
+  { id: "poutcome" },
+  { id: "y" },
+];
+
+export function App() {
+  const ds = useClientDataSource({
+    data: bankDataSmall,
+  });
+
+  const grid = useLyteNytePro({
+    gridId: useId(),
+    rowDataSource: ds,
+    columns,
+    rowGroupModel: ["job", "education"],
+  });
+
+  return (
+    <div>
+      <div style={{ height: 500, display: "flex", flexDirection: "column" }}>
+        <div style={{ flex: "1" }}>
+          <LyteNyteGrid grid={grid} />
+        </div>
+      </div>
+    </div>
+  );
+}
