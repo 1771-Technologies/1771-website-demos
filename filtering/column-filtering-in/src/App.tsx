ae66582ce489077885ee6ae5cf4e552c52ae630e
Lee <leebeydoun@1771technologies.com>
2025-05-10 16:37:57 +0100

    Alpha

---
+++ filtering/column-filtering-in/src/App.tsx
@@ -0,0 +1,60 @@
+import {
+  LyteNyteGrid,
+  useLyteNytePro,
+  useClientDataSource,
+} from "@1771technologies/lytenyte-pro";
+import "@1771technologies/lytenyte-pro/grid.css";
+import { ColumnProReact } from "@1771technologies/lytenyte-pro/types";
+import { bankDataSmall } from "@1771technologies/sample-data/bank-data-smaller";
+import { useId } from "react";
+
+const columns: ColumnProReact[] = [
+  { id: "age", type: "number" },
+  { id: "job" },
+  { id: "balance", type: "number" },
+  { id: "education" },
+  { id: "marital" },
+  { id: "default" },
+  { id: "housing" },
+  { id: "loan" },
+  { id: "contact" },
+  { id: "day", type: "number" },
+  { id: "month" },
+  { id: "duration" },
+  { id: "campaign" },
+  { id: "pdays" },
+  { id: "previous" },
+  { id: "poutcome" },
+  { id: "y" },
+];
+
+export function App() {
+  const ds = useClientDataSource({
+    data: bankDataSmall,
+  });
+
+  const grid = useLyteNytePro({
+    gridId: useId(),
+    rowDataSource: ds,
+    columns,
+
+    filterModel: {
+      job: {
+        set: {
+          kind: "in",
+          columnId: "job",
+          operator: "in",
+          set: new Set(["technician", "services"]),
+        },
+      },
+    },
+  });
+
+  return (
+    <div style={{ height: 500, display: "flex", flexDirection: "column" }}>
+      <div style={{ flex: "1" }}>
+        <LyteNyteGrid grid={grid} />
+      </div>
+    </div>
+  );
+}
---
+++ filtering/column-filtering-quick/README.md
@@ -0,0 +1,7 @@
+# Column Filtering Quick Search
+
+See [Column Filtering Guide](https://1771technologies.com/docs/filter-column-filters).
+
+- [1771 Technologies](https://1771technologies.com)
+- [LyteNyte Pricing](https://1771technologies.com/pricing)
+- [LyteNyte Grid Docs](https://1771technologies.com/docs/intro-getting-started)
